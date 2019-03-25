9b42d3abd2f66a4f654a0bb33413147ba33459a7
AdamG <36753247+AdamG@users.noreply.github.com>
2019-03-24 20:01:43 -0500

    added command line support

--- src/SmiteGuruScraper.py
+++ src/SmiteGuruScraper.py
@@ -21,7 +21,7 @@ class SmiteGuruScraper:
                     page_range  : int           -> The # of pages to scrape. More pages takes longer but yields better results.
                 Returns:
                     BuildItems -> item and active names for the next build."""
-
+        matches_found = 0
         for i in range(1, page_range+1):
             r = requests.get(f"https://smite.guru/profile/{str(id)}-{user}/matches?page={i}")
 
@@ -30,8 +30,9 @@ class SmiteGuruScraper:
                 soup = BeautifulSoup(r.text, 'html.parser')
 
                 # A list of all the match widgets in which the target god was played
-                matches = [match for match in soup.findAll('div', {'class':'match-widget'}) if match.find('div', {'class':'match-widget--title'}).text == god_name] #if match.find(class_="name").string == god_name]
-            
+                matches = [match for match in soup.findAll('div', {'class':'match-widget'}) if match.find('div', {'class':'match-widget--title'}).text == god_name]
+                matches_found += len(matches)
+
                 # Collect the item names for each match
                 for m in matches:
                     item_tags = m.find('div', {'class' : 'match-widget__items'}).findAll('img')
@@ -41,72 +42,6 @@ class SmiteGuruScraper:
                     actives = [active.get('alt') for active in active_tags]
                      
                     yield {"items" : items, "actives" : actives}
+        print(f"Matches Found: {matches_found}")
 
-        
-    # DEPRECATED SINCE smite.guru REDESIGN
-    # @classmethod
-    # def getItemCodes(cls):
-    #     """Scrapes the HTML for the smite.guru item page, to retrieve the number code which corresponds to each item.
-
-    #             Parameters:
-    #                 None
-    #             Returns:
-    #                 Dict[int : str] -> item_code : item_name
-    #     """
-    #     r = requests.get("http://smite.guru/items")
-
-    #     if (r.status_code == 200):
-
-    #         soup = BeautifulSoup(r.text, 'html.parser')
-
-    #         # Get listed items
-    #         items = soup.findAll(class_="card-icon card-md")
-    #         codes_and_names = {int(item["data-item"]) : item.find(class_="name").string for item in items}
-
-    #         # add empty item slot id
-    #         codes_and_names[0] = "EMPTY"
-
-    #         cls.item_codes = codes_and_names
-
-    # @classmethod
-    # def matchItemCode(cls, item_code: int) -> str:
-    #     """Matches an item code to its name.
-
-    #         Parameters:
-    #             item_code : int -> the integer code corresponding to an item on smite.guru
-    #         Returns:
-    #             str -> the name of the item"""
-
-    #     return cls.item_codes[item_code]
-
-    # @classmethod
-    # def saveItemCodes(cls, filename: str):
-    #     """Saves the scraped item codes to a binary file.
-
-    #         Parameters:
-    #             filename : str -> the name of the file
-    #         Returns:
-    #             None
-    #         Throws:
-    #             TypeError -> filename was not a string
-    #     """
-
-    #     if not isinstance(filename, str):
-    #         raise TypeError("filename must be a string.")
-    #     pickle.dump(cls.item_codes, open(filename, "wb"))
-
-    # @classmethod
-    # def loadItemCodes(cls, filename: str):
-    #     """Loads the scraped item codes from a binary file.
-
-    #         Parameters:
-    #             filename : str -> the name of the file
-    #         Returns:
-    #             None
-    #         Throws:
-    #             TypeError -> filename was not a string
-    #     """
-
-    #     if not isinstance(filename, str):
-    #         raise TypeError("filename must be a string.")
-    #     cls.item_codes = pickle.load(open(filename, "rb"))
+    
--- src/main.py
+++ src/main.py
@@ -1,10 +1,18 @@
 from BuildCreator import BuildCreator
 from SmiteGuruScraper import SmiteGuruScraper
+import argparse
+
+parser = argparse.ArgumentParser(description='Generate a build for a smite god.')
+parser.add_argument('god_name', type=str,
+                    help='The name of the god to create a build for.')
+parser.add_argument('user', type=str, help='The name of the user whose build you want to search.')
+parser.add_argument('id', type=int, help='The user id from smite.guru (temporary during development)')
+parser.add_argument('-p', type=int, help='page range to search from smite.guru', default=10)
+args = parser.parse_args()
 
 if __name__ == '__main__':
-    god_name = "Jormungandr"
-    g = SmiteGuruScraper.getBuildInfo(god_name, "Wlfy", 873380, page_range=7)
-    bc = BuildCreator(god_name)
+    g = SmiteGuruScraper.getBuildInfo(args.god_name, args.user, args.id, page_range=args.p)
+    bc = BuildCreator(args.god_name)
     for b in g:
         bc.addBuild(b)
 
